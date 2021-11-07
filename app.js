740f206ac4b0b50cf58c451640a19766e35aa685
unknown <diegolepel@hotmail.com>
2021-11-07 01:42:45 -0600

    dddddf

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+var cors = require('cors');
+app.use(cors());
+
+app.use('/home', express.static(__dirname + '/home.html'));
+app.use('/folders', express.static(__dirname + '/folders.html'));
+app.use('/profile', express.static(__dirname + '/profile.html'));
+
+app.use(express.json());
+
+//authMiddleware
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
+
